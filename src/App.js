96264b9ada8046311d8d31210cd1feee6224f5b9
AjayShanmugasundram <ajay.shanmugasundram@agilisium.com>
2022-10-10 16:33:51 +0530

    implemented viewpatienttable viewpatienttask in fetchpatient task

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import S2 from './components/S2/S2';
 import S3 from './components/S3/S3';
 import Compare from './components/Compare/Compare';
 import React, { useMemo, useState } from "react";
+import FetchPatientTask from './components/fetchPatientTask/FetchPateintTask';
+
 
 function App() {
   // const [COIValue1, setCOIValue1] = useState();
   // const [COIValue2, setCOIValue2] = useState();
 
   return (
-    <div className='flex flex-col w-screen h-screen p-10 bg-gray-200'>
+    <div className='flex flex-col w-screen h-screen p-10 '>
       <div className='h-1/5 align-middle flex'><S1 /></div>
       <hr className=' border-black' />
       <div className='flex flex-row h-4/5 bg-gray-100'>
@@ -27,6 +29,8 @@ function App() {
           <div className=' h-1/5'><Compare /></div>
         </div>
       </div>
+      <br></br>
+      <FetchPatientTask/>
     </div>
   )
 }
---
+++ src/components/viewPatientTask/ViewPatientTask.js
@@ -0,0 +1,70 @@
+import React from 'react'
+export default function ViewPatientTask({ fetchedPatientTask, status }) {
+  if (status === 200) {
+    return (
+      <>
+        <div className='overflow-x-auto relative my-3 sm:rounded-lg'>
+          <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+            <thead className='text-xs text-white  bg-blue-900 dark:bg-gray-700 dark:text-gray-400'>
+              <tr>
+                <th scope='col' className='py-3 px-6'>
+                  Execution Order
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  Process Name
+                </th>
+                <th scope='col' className='py-3 px-7'>
+                  Status
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {fetchedPatientTask.map((fetchedPatientTask, i) => {
+                return (
+                  <tr
+                    className={
+                      fetchedPatientTask.Status == 'WIP'
+                        ? 'bg-blue-200   dark:bg-gray-900 dark:border-gray-700 hover:bg-blue-300 border-blue-500'
+                        : fetchedPatientTask.Status=='Validation' ?'bg-gray-100 border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100' :'bg-white border-b text-black dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100'
+                    }
+                  >
+                    <td className='py-4 px-6'>
+                      {fetchedPatientTask.ExecutionOrder}
+                    </td>
+                    <td className='py-4 px-6'>
+                      {fetchedPatientTask.Task.ProcessName}
+                    </td>
+                    {fetchedPatientTask.Status == 'Completed' ? (
+                      <td className='py-4 px-6 '>
+                        <span className='bg-green-100 text-green-500 border border-green-500 rounded px-1 py-1'>
+                          {fetchedPatientTask.Status}
+                        </span>
+                      </td>
+                    ) : fetchedPatientTask.Status == 'WIP' ? (
+                      <td className='py-4 px-6 text-green-900'>
+                        <div className='flex flex-row flex-auto gap-9'>
+                          {/* <span className='bg-yellow-100 text-yellow-500 border border-yellow-500 rounded px-6 py-1'>
+                            {fetchedPatientTask.Status}
+                          </span>{' '} */}
+                          <button className='px-5 py-1 bg-blue-500 text-white border rounded border-blue-700 uppercase'>Scan</button>{' '}
+                        </div>
+                      </td>
+                    ) : fetchedPatientTask.Status == 'Validation' ? (
+                      <td className='py-4 px-6 text-grey-900'>
+                        <span className='bg-grey-100 text-grey-500 border border-black rounded  px-3 py-1'>
+                        Pending
+                        </span>
+                      </td>
+                    ) : null}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    )
+  }
+
+  return null
+}
